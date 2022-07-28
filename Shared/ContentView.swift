0f8a83d8df66b8ce140e5f86cf87915182952a6b
venskoA <venskoandrey@gmail.com>
2022-07-28 20:42:59 +0300

    main class add exercises

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -16,7 +16,8 @@ struct ContentView: View {
         .onAppear {
 //            FirstHW()
 //            SecondHW()
-            ThirdHW()
+//            ThirdHW()
+            FifthHW()
         }
     }
 }
